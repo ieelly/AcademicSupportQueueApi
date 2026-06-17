ad882af816f60a7137e827533d7616c0d5ce14fa
ieelly <raniiellyfernandes@gmail.com>
2026-06-17 16:43:01 -0300

    Atualiza conexão e adiciona TipoSolicitacao
    - Atualiza string de conexão no `appsettings.json` para usar autenticação com usuário e senha.
    - Adiciona propriedade `TipoSolicitacao` na entidade `Atendimento`.
    - Atualiza `ContextoBancoModelSnapshot.cs` com nova propriedade e versão do produto.
    - Adiciona `docker-compose.yml` para configurar contêiner SQL Server.
    - Cria migração para adicionar coluna `TipoSolicitacao` na tabela `Atendimentos`.

--- src/AcademicSupportQueueApi.Infrastructure/Migrations/20260617191459_AdicionaTipoSolicitacao.Designer.cs
+++ src/AcademicSupportQueueApi.Infrastructure/Migrations/20260617191459_AdicionaTipoSolicitacao.Designer.cs
@@ -4,6 +4,7 @@ using AcademicSupportQueueApi.Infrastructure.Dados;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademicSupportQueueApi.Infrastructure.Migrations
 {
     [DbContext(typeof(ContextoBanco))]
-    partial class ContextoBancoModelSnapshot : ModelSnapshot
+    [Migration("20260617191459_AdicionaTipoSolicitacao")]
+    partial class AdicionaTipoSolicitacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
